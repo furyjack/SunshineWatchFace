206014937dba079f04562e199d9d484ae3d63c08
furyjack <lakshaytaneja26@gmail.com>
2016-12-23 12:12:08 +0530

    basic struct working

--- S12.04-Solution-ResourceQualifiers/wear/src/main/java/com/example/wear/SimpleFace.java
+++ S12.04-Solution-ResourceQualifiers/wear/src/main/java/com/example/wear/SimpleFace.java
@@ -1,10 +1,26 @@
 package com.example.wear;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
 import android.view.SurfaceHolder;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class SimpleFace extends CanvasWatchFaceService {
 
@@ -15,42 +31,252 @@ public class SimpleFace extends CanvasWatchFaceService {
     }
 
 
+
     private class Engine extends CanvasWatchFaceService.Engine {
+
+        static final int MSG_UPDATE_TIME = 0;
+        private static final long INTERACTIVE_UPDATE_RATE_MS = 1000;
+
+        Calendar mCalendar;
+
+        // device features
+        boolean mLowBitAmbient;
+
+
+        // graphic objects
+        Bitmap mBackgroundBitmap;
+        Bitmap mBackgroundScaledBitmap;
+        Paint mHourPaint;
+        Paint mMinutePaint;
+        private boolean mRegisteredTimeZoneReceiver=false;
+        private boolean mBurnInProtection;
+        Bitmap mGrayBackgroundBitmap;
+
+        private void updateTimer() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
+            if (shouldTimerBeRunning()) {
+                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
+
+
+
+        final Handler mUpdateTimeHandler = new Handler() {
+            @Override
+            public void handleMessage(Message message) {
+                switch (message.what) {
+                    case MSG_UPDATE_TIME:
+                        invalidate();
+                        if (shouldTimerBeRunning()) {
+                            long timeMs = System.currentTimeMillis();
+                            long delayMs = INTERACTIVE_UPDATE_RATE_MS
+                                    - (timeMs % INTERACTIVE_UPDATE_RATE_MS);
+                            mUpdateTimeHandler
+                                    .sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+                        }
+                        break;
+                }
+            }
+        };
+        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mCalendar.setTimeZone(TimeZone.getDefault());
+                invalidate();
+            }
+        };
+
+        @Override
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            if (mBackgroundScaledBitmap == null
+                    || mBackgroundScaledBitmap.getWidth() != width
+                    || mBackgroundScaledBitmap.getHeight() != height) {
+                if(!isInAmbientMode())
+                mBackgroundScaledBitmap = Bitmap.createScaledBitmap(mBackgroundBitmap,
+                        width, height, true /* filter */);
+                else
+                    mBackgroundScaledBitmap = Bitmap.createScaledBitmap(mGrayBackgroundBitmap,
+                            width, height, true /* filter */);
+
+            }
+            super.onSurfaceChanged(holder, format, width, height);
+
+
+        }
 
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
             /* initialize your watch face */
+            Resources resources = SimpleFace.this.getResources();
+            Drawable backgroundDrawable = resources.getDrawable(R.drawable.bg, null);
+            mBackgroundBitmap = ((BitmapDrawable) backgroundDrawable).getBitmap();
+            mGrayBackgroundBitmap=((BitmapDrawable)resources.getDrawable(R.drawable.bgg,null)).getBitmap();
+
+            // create graphic styles
+            mHourPaint = new Paint();
+            mHourPaint.setARGB(255, 200, 200, 200);
+            mHourPaint.setStrokeWidth(5.0f);
+            mHourPaint.setAntiAlias(true);
+            mHourPaint.setStrokeCap(Paint.Cap.ROUND);
+            mMinutePaint=new Paint(mHourPaint);
+
+
+
+            // allocate a Calendar to calculate local time using the UTC time and time zone
+            mCalendar = Calendar.getInstance();
+            setWatchFaceStyle(new WatchFaceStyle.Builder(SimpleFace.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+                    .setBackgroundVisibility(WatchFaceStyle
+                            .BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .build());
+
+            
         }
 
         @Override
         public void onPropertiesChanged(Bundle properties) {
             super.onPropertiesChanged(properties);
             /* get device features (burn-in, low-bit ambient) */
+            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
+            mBurnInProtection = properties.getBoolean(PROPERTY_BURN_IN_PROTECTION,
+                    false);
         }
 
         @Override
         public void onTimeTick() {
             super.onTimeTick();
             /* the time changed */
+            invalidate();
         }
 
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
             /* the wearable switched between modes */
+            if (mLowBitAmbient) {
+                boolean antiAlias = !inAmbientMode;
+                mHourPaint.setAntiAlias(antiAlias);
+                mMinutePaint.setAntiAlias(antiAlias);
+
+
+            }
+            invalidate();
+            updateTimer();
         }
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
             /* draw your watch face */
+            mCalendar.setTimeInMillis(System.currentTimeMillis());
+
+            // Constant to help calculate clock hand rotations
+            final float TWO_PI = (float) Math.PI * 2f;
+
+            int width = bounds.width();
+            int height = bounds.height();
+            if (isInAmbientMode() && (mLowBitAmbient || mBurnInProtection)) {
+                canvas.drawColor(Color.BLACK);
+            } else if (isInAmbientMode()) {
+                canvas.drawBitmap(mBackgroundScaledBitmap, 0, 0, null);
+            } else {
+                canvas.drawBitmap(mBackgroundScaledBitmap, 0, 0, null);
+            }
+
+          //  canvas.drawBitmap(mBackgroundScaledBitmap, 0, 0, null);
+
+            // Find the center. Ignore the window insets so that, on round watches
+            // with a "chin", the watch face is centered on the entire screen, not
+            // just the usable portion.
+            float centerX = width / 2f;
+            float centerY = height / 2f;
+
+            // Compute rotations and lengths for the clock hands.
+            float seconds = mCalendar.get(Calendar.SECOND) +
+                    mCalendar.get(Calendar.MILLISECOND) / 1000f;
+            float secRot = seconds / 60f * TWO_PI;
+            float minutes = mCalendar.get(Calendar.MINUTE) + seconds / 60f;
+            float minRot = minutes / 60f * TWO_PI;
+            float hours = mCalendar.get(Calendar.HOUR) + minutes / 60f;
+            float hrRot = hours / 12f * TWO_PI;
+
+            float secLength = centerX - 20;
+            float minLength = centerX - 40;
+            float hrLength = centerX - 80;
+
+            // Only draw the second hand in interactive mode.
+//            if (!isInAmbientMode()) {
+//                float secX = (float) Math.sin(secRot) * secLength;
+//                float secY = (float) -Math.cos(secRot) * secLength;
+//                canvas.drawLine(centerX, centerY, centerX + secX, centerY +
+//                        secY, mSecondPaint);
+//            }
+
+            // Draw the minute and hour hands.
+            float minX = (float) Math.sin(minRot) * minLength;
+            float minY = (float) -Math.cos(minRot) * minLength;
+            canvas.drawLine(centerX, centerY, centerX + minX, centerY + minY,
+                    mMinutePaint);
+            float hrX = (float) Math.sin(hrRot) * hrLength;
+            float hrY = (float) -Math.cos(hrRot) * hrLength;
+            canvas.drawLine(centerX, centerY, centerX + hrX, centerY + hrY,
+                    mHourPaint);
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
             /* the watch face became visible or invisible */
+
+            if (visible) {
+                registerReceiver();
+
+                // Update time zone in case it changed while we weren't visible.
+                mCalendar.setTimeZone(TimeZone.getDefault());
+            } else {
+                unregisterReceiver();
+            }
+
+            // Whether the timer should be running depends on whether we're visible and
+            // whether we're in ambient mode, so we may need to start or stop the timer
+            updateTimer();
         }
+
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
+        }
+
+        private void registerReceiver() {
+
+            if (mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = true;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            SimpleFace.this.registerReceiver(mTimeZoneReceiver, filter);
+        }
+
+
+        private void unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = false;
+            SimpleFace.this.unregisterReceiver(mTimeZoneReceiver);
+        }
+
+
+
+
     }
+
+
+
+
+
+
+
 
 }
